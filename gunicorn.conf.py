4d8c06d474940e0bbc6f51c179fe9d7eecfda925
sustachio <88939526+sustachio@users.noreply.github.com>
2023-05-08 21:41:59 -0500

    Feature: add files for rpi server ( not the keys (; )

---
+++ gunicorn.conf.py
@@ -0,0 +1,15 @@
+from glob import glob
+
+# sudo gunicorn -c /home/seth/blog/gunicorn.conf.py
+
+chdir = "/home/seth/blog"
+
+wsgi_app = "main:app"
+bind = "0.0.0.0:8443"
+worker_class = "gevent"
+
+reload = True
+reload_extra_files = glob("/home/seth/blog/templates/*") + glob("/home/seth/blog/posts_md/*") + glob("/home/seth/blog/static/*") + glob("/home/seth/blog/*.py")
+
+certfile = "/home/seth/blog/fullchain.pem"
+keyfile = "/home/seth/blog/privkey.pem"
